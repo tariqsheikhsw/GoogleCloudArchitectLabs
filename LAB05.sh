26da44810db6a5a7cfd160f7369f4224f1e586b3
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-03-31 08:47:00 +0400

    Update LAB05.sh

--- LAB05.sh
+++ LAB05.sh
@@ -185,7 +185,6 @@ while true; do
     break
   else
     echo "Waiting for Cloud Run service to be created..."
-    echo "Meantime Subscribe to Quicklab[https://www.youtube.com/@quick_lab]."
     sleep 10
   fi
 done
