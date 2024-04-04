806b15239c445014f2b1eecf815e14e343625ec6
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-04-04 14:24:43 +0400

    Create GSP354.sh

---
+++ GSP354.sh
@@ -0,0 +1,21 @@
+Vertex AI Instance :
+
+git clone https://github.com/GoogleCloudPlatform/training-data-analyst
+
+cd training-data-analyst/quests/vertex-ai/vertex-challenge-lab
+pip install -U -r requirements.txt --user
+
+//training-data-analyst/quests/vertex-ai/vertex-challenge-lab
+//vertex-challenge-lab.ipynb : TensorFlow 2-11
+
+PROJECT_ID, REGION, and GCS_BUCKET 
+
+PROJECT_ID = "qwiklabs-gcp-04-0513bb6e50d2"
+REGION = "us-west1"
+GCS_BUCKET = f"gs://qwiklabs-gcp-04-0513bb6e50d2-vertex-challenge-lab"
+
+
+
+
+
+
