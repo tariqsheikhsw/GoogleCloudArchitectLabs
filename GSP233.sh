2f81813697fa1a2ec4ac1da65abcc6852cdae90e
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-04-13 08:49:43 +0400

    Create GSP233.sh

---
+++ GSP233.sh
@@ -0,0 +1,11 @@
+
+export REGION="${ZONE%-*}"
+
+gsutil -m cp -r gs://spls/gsp233/* .
+
+cd tf-gke-k8s-service-lb
+
+terraform init
+
+terraform apply -var="region=$REGION" -var="location=$ZONE" --auto-approve
+
