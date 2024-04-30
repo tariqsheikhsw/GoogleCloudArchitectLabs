63d616d6ae21103b643fe84a0f3cd48a3bcc47ca
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-04-30 13:58:47 +0400

    Update main.tf

--- Solutions/GSPbbb/instance/main.tf
+++ Solutions/GSPbbb/instance/main.tf
@@ -1,1 +1,28 @@
+variable "instance_name" {}
+variable "instance_zone" {}
 
+variable "instance_type" {
+  default = "e2-micro"
+}
+
+variable "instance_subnetwork" {}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "${var.instance_name}"
+  zone         = "${var.instance_zone}"
+  machine_type = "${var.instance_type}"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    subnetwork = "${var.instance_subnetwork}"
+
+    access_config {
+      # Allocate a one-to-one NAT IP to the instance
+    }
+  }
+}
