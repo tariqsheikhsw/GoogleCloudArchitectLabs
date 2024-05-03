11ae1c4901fd9d7f91ebcc3a05b3b8b963617e3b
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-05-03 17:53:11 +0400

    Create GSP199Task1.sh

---
+++ Solutions/GSP199Task1.sh
@@ -0,0 +1,23 @@
+
+
+gcloud auth list
+export PROJECT_ID=$(gcloud config get-value project)
+
+export REGION=${ZONE%-*}
+gcloud config set compute/region $REGION
+
+gcloud iam service-accounts create my-sa-123 --display-name "my service account"
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member serviceAccount:my-sa-123@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --role roles/editor
+
+# Create a service account
+gcloud iam service-accounts create bigquery-qwiklab --description="please like share & subscribe to techcps" --display-name="bigquery-qwiklab"
+
+# Assign BigQuery Data Viewer role
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member="serviceAccount:bigquery-qwiklab@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com" --role="roles/bigquery.dataViewer"
+
+# Assign BigQuery User role
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member="serviceAccount:bigquery-qwiklab@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com" --role="roles/bigquery.user"
+
+# Create a VM instance with the following information:
+gcloud compute instances create bigquery-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --service-account=bigquery-qwiklab@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --create-disk=auto-delete=yes,boot=yes,device-name=bigquery-instance,image=projects/debian-cloud/global/images/debian-11-bullseye-v20231010,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
