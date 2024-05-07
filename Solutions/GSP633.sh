f0fec6482b2895a23225482e3eff5298697c298d
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-05-07 19:02:27 +0400

    Create GSP633.sh

---
+++ Solutions/GSP633.sh
@@ -0,0 +1,13 @@
+
+gcloud services enable \
+  compute.googleapis.com \
+  monitoring.googleapis.com \
+  logging.googleapis.com \
+  notebooks.googleapis.com \
+  aiplatform.googleapis.com \
+  artifactregistry.googleapis.com \
+  container.googleapis.com
+
+
+Replace ProjectID value and run the notebook
+//CLS_Vertex_AI_CNN_fmnist.ipynb
