eabd5c4229380ccfd3656424457bdf4db12086b1
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-05-08 04:25:03 +0400

    Create GSPkkk.sh

---
+++ Solutions/GSPkkk.sh
@@ -0,0 +1,76 @@
+
+
+gcloud auth list
+
+gcloud services enable cloudfunctions.googleapis.com
+
+sleep 20
+
+git clone --depth=1 https://github.com/GoogleCloudPlatform/training-data-analyst
+
+ln -s ~/training-data-analyst/courses/developingapps/v1.3/nodejs/cloudfunctions ~/cloudfunctions
+
+cd ~/cloudfunctions/start
+
+. prepare_environment.sh
+
+
+mkdir gsplab && cd gsplab
+
+
+
+cat > index.js <<EOF_END
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.helloWorld = (req, res) => {
+    let message = req.query.message || req.body.message || 'Hello World!';
+    res.status(200).send(message);
+  };
+  
+EOF_END
+
+cat > package.json <<EOF_END
+{
+    "name": "sample-http",
+    "version": "0.0.1"
+  }
+  
+EOF_END
+
+
+
+  gcloud functions deploy process-feedback \
+    --runtime nodejs20 \
+    --entry-point helloWorld \
+    --source . \
+    --trigger-topic feedback \
+    --max-instances 1 \
+    --no-allow-unauthenticated \
+    --quiet 
+
+cd ..
+
+cd function
+
+rm index.js
+
+wget https://raw.githubusercontent.com/tariqsheikhsw/GoogleCloudArchitectLabs/main/Source/index.js
+
+zip cf.zip *.js*
+
+gcloud storage cp cf.zip gs://$GCLOUD_BUCKET/
+
+
+
+gcloud functions deploy process-feedback \
+    --runtime nodejs20 \
+    --entry-point subscribe \
+    --trigger-topic feedback \
+    --max-instances 1 \
+    --no-allow-unauthenticated \
+    --source gs://$GCLOUD_BUCKET/cf.zip \
+    --allow-unauthenticated
