11a58d8d1e997c5d8e4b5c173806e4c7dcdf9ec5
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-05-10 07:20:19 +0400

    Update GSP1110.sh

--- Solutions/GSP1110.sh
+++ Solutions/GSP1110.sh
@@ -1,1 +1,20 @@
+echo "export ZONE=us-central1-a" >> ~/.bashrc
+echo "export REGION=us-central1" >> ~/.bashrc
+echo "export PROJECT_ID=`gcloud config get-value core/project`" >> ~/.bashrc
+echo "export BACKUP_PLAN=my-backup-plan" >> ~/.bashrc
+source ~/.bashrc
+echo "export EXTERNAL_ADDRESS=$(gcloud compute addresses describe app-address --format='value(address)' --region $REGION)" >> ~/.bashrc
+source ~/.bashrc
+
+gcloud services enable gkebackup.googleapis.com
+
+gcloud beta container clusters update lab-cluster \
+--project=$PROJECT_ID  \
+--update-addons=BackupRestore=ENABLED \
+--zone=$ZONE
+
+gcloud beta container clusters describe lab-cluster \
+--project=$PROJECT_ID  \
+--zone=$ZONE | grep -A 1 gkeBackupAgentConfig:
+
 
