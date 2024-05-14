49bef14adb32c5123da1d22dd439e7f55bec4866
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-05-14 10:57:46 +0400

    Update GSP616.sh

--- Solutions/GSP616.sh
+++ Solutions/GSP616.sh
@@ -1,1 +1,13 @@
 
+terraform -v
+
+git clone https://github.com/terraform-google-modules/cloud-foundation-training.git
+
+cd cloud-foundation-training/other/terraform-codelab/lab-networking
+
+//Replace your PROJECT ID 
+
+cat > terraform.tfvars <<EOF_END
+project_id=qwiklabs-gcp-03-8dca2649c476
+EOF_END
+
