7c20f26234b86ce55a425ebb00ba1ad6648f4b4f
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-05-15 11:31:17 +0400

    Update GSP864.sh

--- Solutions/GSP864.sh
+++ Solutions/GSP864.sh
@@ -4,10 +4,10 @@ export BUCKET_NAME=$DEVSHELL_PROJECT_ID-bucket
 
 export PROJECT_ID=$DEVSHELL_PROJECT_ID
 
-//git clone https://github.com/quiccklabs/Redacting-Sensitive-Data-with-Cloud-Data-Loss-Prevention.git
+#git clone https://github.com/quiccklabs/Redacting-Sensitive-Data-with-Cloud-Data-Loss-Prevention.git
 git clone https://github.com/googleapis/synthtool
 
-//cd Redacting-Sensitive-Data-with-Cloud-Data-Loss-Prevention/quicklabgsp864/samples && npm install
+#cd Redacting-Sensitive-Data-with-Cloud-Data-Loss-Prevention/quicklabgsp864/samples && npm install
 cd synthtool/tests/fixtures/nodejs-dlp/samples/ && npm install
 
 gcloud config set project $PROJECT_ID
