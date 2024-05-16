12a7653b17369a2016a201d215acd31d17ce84ce
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-05-16 09:16:58 +0400

    Create GSPnnn.sh

---
+++ Solutions/GSPnnn.sh
@@ -0,0 +1,15 @@
+
+
+gcloud auth list
+
+git clone https://github.com/terraform-google-modules/docs-examples.git
+
+cd docs-examples/firewall_basic/
+
+terraform init
+
+terraform apply --auto-approve
+
+terraform init
+
+terraform apply --auto-approve
