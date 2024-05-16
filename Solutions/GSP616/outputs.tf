e0effea2fcc27767752373748f7b0ede49354f00
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-05-16 16:04:07 +0400

    Update outputs.tf

--- Solutions/GSP616/outputs.tf
+++ Solutions/GSP616/outputs.tf
@@ -14,3 +14,16 @@
  * limitations under the License.
  */
 
+output "network_name" {
+  value = module.vpc.network_name
+}
+
+output "first_subnet_name" {
+  value = module.vpc.subnets_names[0]
+}
+
+# Add you new output below this line
+# Add you new output below this line
+output "my_subnet_name" {
+  value = module.vpc.subnets_names[2]
+}
