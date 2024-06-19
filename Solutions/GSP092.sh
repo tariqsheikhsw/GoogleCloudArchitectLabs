8ae93fdba5b9407dab5802fb184708c3577f5eae
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-06-19 11:07:11 +0400

    Update GSP092.sh

--- Solutions/GSP092.sh
+++ Solutions/GSP092.sh
@@ -52,5 +52,5 @@ tar xvzf vegeta-v6.3.0-linux-386.tar.gz
 
 gcloud logging metrics create CloudFunctionLatency-Logs \
     --project=$DEVSHELL_PROJECT_ID \
-    --description="subscribe to quicklab" \
+    --description="Google CSB" \
     --log-filter='resource.type="cloud_function" AND resource.labels.function_name="helloWorld" AND log_name="projects/$DEVSHELL_PROJECT_ID/logs/cloudaudit.googleapis.com%2Factivity" AND resource.labels.region="$REGION"'
