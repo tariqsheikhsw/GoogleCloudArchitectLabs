c7d5978929a67b312107788528a3c564afa3155e
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2024-07-25 22:06:13 +0400

    Create arc130.sh

---
+++ Solutions/arc130.sh
@@ -0,0 +1,28 @@
+cat > analyze-request.json <<EOF_END
+{
+  "document":{
+    "type":"PLAIN_TEXT",
+    "content": "Google, headquartered in Mountain View, unveiled the new Android phone at the Consumer Electronic Show.  Sundar Pichai said in his keynote that users love their new Android phones."
+  },
+  "encodingType": "UTF8"
+}
+EOF_END
+
+curl -s -H "Content-Type: application/json" \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+"https://language.googleapis.com/v1/documents:analyzeSyntax" \
+-d @analyze-request.json > analyze-response.txt
+
+cat > multi-nl-request.json <<EOF_END
+{
+  "document":{
+    "type":"PLAIN_TEXT",
+    "content":"Le bureau japonais de Google est situé à Roppongi Hills, Tokyo."
+  }
+}
+EOF_END
+
+curl -s -H "Content-Type: application/json" \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+"https://language.googleapis.com/v1/documents:analyzeEntities" \
+-d @multi-nl-request.json > multi-response.txt
